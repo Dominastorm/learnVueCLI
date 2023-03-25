86ec8436b53920874c750a8ae59b0c1653ffab1b
Dominastorm <dchawla228@gmail.com>
2023-03-25 14:37:15 +0530

    Add Past Orders page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductsView from '../views/ProductsView.vue'
+import PastOrdersView from '../views/PastOrdersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/products',
       name: 'products',
       component: ProductsView
+    },
+    {
+      path: '/past-orders',
+      name: 'pastOrders',
+      component: PastOrdersView
     }
   ]
 })
